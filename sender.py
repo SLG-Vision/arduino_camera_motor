58c9bf42d61ebaa606ecbc279fa0a7cb437ce29f
Giacomo Salici <giambo2000@gmail.com>
2023-06-06 20:32:22 +0200

    class

--- sender.py
+++ sender.py
@@ -1,12 +1,24 @@
 import serial
 import argparse
 
-ser = serial.Serial('/dev/tty.usbmodem1201', 9600)  
 
+
+
+
+class MySerial():
+    def __init__(self, serial_port: str = "/dev/tty.usbmodem1201") -> None:
+        self.serial = serial.Serial(serial_port, 9600)  
+
+    def write(self, number):
+        self.serial.write(number.encode())
+
+    def close(self):
+        self.serial.close()
+
+
+ser = MySerial()
 parser = argparse.ArgumentParser(description='Send a number to Arduino')
 parser.add_argument('number', type=int, help='The number to send to Arduino')
 args = parser.parse_args()
-
-ser.write(str(args.number).encode())
-
+ser.write(str(args.number))
 ser.close()
